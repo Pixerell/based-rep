abfa1f02fe2801bf1029fc2eb32d2fd263751e40
Pavel Marchenko <pavlushok38@gmail.com>
2022-09-13 14:49:53 +0300

    First commit

---
+++ src/components/main-page/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import logo from '../../logo.svg';
+import Header from "./Header";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main-page/Header.tsx
@@ -0,0 +1,18 @@
+import {
+	AppBar,
+	Toolbar
+} from "@mui/material";
+
+
+function Header(): JSX.Element {
+	return (
+		<AppBar>
+			<Toolbar>
+
+			</Toolbar>
+		</AppBar>
+	)
+}
+
+
+export default Header;
