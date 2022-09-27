d1b87f8bd31bcb9b95928534781e48870add1e17
Pavel Marchenko <pavlushok38@gmail.com>
2022-09-27 14:33:38 +0300

    Создан Header, обновлен readme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../../App.css';
-import logo from '../../logo.svg';
-import Header from "./Header";
+import './App.css';
+import Header from "./components/main-page/Header";
+
 
 function App(): JSX.Element {
   return (
--- src/components/main-page/Header.tsx
+++ src/components/main-page/Header.tsx
@@ -1,18 +1,28 @@
+import {AccessibleForwardOutlined} from "@mui/icons-material";
 import {
 	AppBar,
-	Toolbar
+	Icon,
+	Toolbar, Typography
 } from "@mui/material";
+import React from 'react';
 
 
 function Header(): JSX.Element {
 	return (
 		<AppBar>
-			<Toolbar>
-
+			<Toolbar className={'Header'}>
+				<Icon>
+					<AccessibleForwardOutlined/>
+				</Icon>
+				<Typography
+					ml={'2.5vh'}
+					fontWeight={'650'}
+				>
+					Based Place
+				</Typography>
 			</Toolbar>
 		</AppBar>
 	)
 }
-
 
 export default Header;
