1e12dd34a1a687bf58d985fc49486b4a445efa16
Pavel Marchenko <pavlushok38@gmail.com>
2022-09-28 12:39:50 +0300

    BT-13 - added custom theme palette for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
+import {ThemeOptions} from "@mui/material";
 import React from 'react';
 import './App.css';
+import MainContent from "./components/main-page/Content";
 import Header from "./components/main-page/Header";
 
+export const themeOptions: ThemeOptions = {
+	palette: {
+		background: {
+			default: '#180c38',
+			paper: '#200c36',
+		},
+		text: {
+			primary: '#ffffff',
+			secondary: '#a7a7a7',
+			disabled: '#5d5d5d',
+		},
+		primary: {
+			main: '#2c0c33',
+		},
+		secondary: {
+			main: '#503657',
+		},
+		error: {
+			main: '#4D0A0D',
+			light: '#521A24',
+		},
+		warning: {
+			main: '#671264',
+			light: '#7B3278',
+		},
+		success: {
+			main: '#034B6A',
+		}
+	}
+}
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header/>
+			<MainContent/>
+		</div>
+	);
 }
 
 export default App;
