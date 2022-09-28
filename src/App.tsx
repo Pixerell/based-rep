496171206052f870df240fe3e70040eb80066137
Pavel Marchenko <pavlushok38@gmail.com>
2022-09-28 17:14:01 +0300

    Леша гей

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {createTheme, ThemeOptions} from "@mui/material";
 import React from 'react';
 import './App.css';
+import MainContent from "./components/main-page/Content";
 import Header from "./components/main-page/Header";
 
 export default function App(): JSX.Element {
 	return (
 		<div>
 			<Header/>
+			<MainContent/>
 		</div>
 	);
 }
