1e47415ffc1a98182ecf3532726e0a0e28e91b19
Pixerell <PinkDriftsA@protonmail.com>
2022-09-30 21:50:44 +0300

    BT-21 just routed this whole mf

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import MainContent from "./components/main-page/Content";
 import Header from "./components/main-page/Header";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Base from "./components/base/Base";
+import Talant from "./components/talant/Talant";
+import Graphics from "./components/graphics/Graphics";
+
 
 export default function App(): JSX.Element {
 	return (
 		<div>
-			<Header/>
-			<MainContent/>
+			<Router>
+				<Header/>
+				<Routes>
+					<Route  path="/" element={<MainContent/>}/>
+					<Route path="/base" element={<Base/>}/>
+					<Route path="/talant" element={<Talant/>}/>
+					<Route path="/graphics" element={<Graphics/>}/>
+				</Routes>
+
+			</Router>
 		</div>
 	);
 }
