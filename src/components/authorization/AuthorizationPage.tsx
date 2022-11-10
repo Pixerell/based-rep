6ff569f17fae83ca59d10a46eeda86605936737e
Pavel Marchenko <pavlushok38@gmail.com>
2022-11-10 17:03:09 +0300

    fix(multiple) - Добавлены импорты

---
+++ src/components/authorization/AuthorizationPage.tsx
@@ -0,0 +1,9 @@
+import Authorization from "./Authorization";
+
+export default function AuthorizationPage(): JSX.Element {
+	return (
+		<div>
+			<Authorization/>
+		</div>
+	)
+}
--- src/components/talant/TalantPage.tsx
+++ src/components/talant/TalantPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from "../main-page/Header";
 import Talant from "./Talant";
 
