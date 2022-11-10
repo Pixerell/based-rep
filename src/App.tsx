608cef6368e18440cda2e4041d5355257fa4087b
Pavel Marchenko <pavlushok38@gmail.com>
2022-11-10 17:00:28 +0300

    fix(multiple) - BT-35 - Отныне под каждый компонент, сперва создается page, а после весь контент в него, а не как раньше

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import './App.css';
-import MainContent from "./components/main-page/Content";
-import Header from "./components/main-page/Header";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Base from "./components/base/Base";
-import Talant from "./components/talant/Talant";
-import Graphics from "./components/graphics/Graphics";
+import './App.css';
+import BasePage from "./components/base/BasePage";
+import GraphicsPage from "./components/graphics/GraphicPage";
+import MainPage from "./components/main-page/MainPage";
+import TalantPage from "./components/talant/TalantPage";
 
 
 export default function App(): JSX.Element {
 	return (
 		<div>
 			<Router>
-				<Header/>
 				<Routes>
-					<Route  path="/" element={<MainContent/>}/>
-					<Route path="/base" element={<Base/>}/>
-					<Route path="/talant" element={<Talant/>}/>
-					<Route path="/graphics" element={<Graphics/>}/>
+					<Route path="/" element={<MainPage/>}/>
+					<Route path="/base" element={<BasePage/>}/>
+					<Route path="/talant" element={<TalantPage/>}/>
+					<Route path="/graphics" element={<GraphicsPage/>}/>
+					<Route/>
 				</Routes>
-
 			</Router>
 		</div>
 	);
---
+++ src/components/main-page/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainContent from "./Content";
+import Header from "./Header";
+
+export default function MainPage(): JSX.Element {
+	
+	return (
+		<div>
+			<Header/>
+			<MainContent/>
+		</div>
+	) 
+}
