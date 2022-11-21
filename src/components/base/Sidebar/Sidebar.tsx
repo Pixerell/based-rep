8263009910378045455d69bc7e71fdb525838cea
Pavel Marchenko <pavlushok38@gmail.com>
2022-11-21 13:01:15 +0300

    dev(fix)- Навел красоту

--- src/components/base/Sidebar/Sidebar.tsx
+++ src/components/base/Sidebar/Sidebar.tsx
@@ -6,59 +6,59 @@ import './Sidebar.scss';
 import {SidebarData} from "./SidebarData";
 
 export default function Sidebar(): JSX.Element {
-    const [sidebar, setSidebar] = useState(false)
+	const [sidebar, setSidebar] = useState(false)
 
-    window.onscroll = () => {scrollFunction()};
+	window.onscroll = () => {
+		scrollFunction()
+	};
 
-    function scrollFunction() : void {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-        {
-            // @ts-ignore
-            document.getElementById("side").style.top = "0";
-            console.log('moved?')
-        }
-        else {
-            // @ts-ignore
-            document.getElementById("side").style.top = "70px";
-        }
-    }
+	function scrollFunction(): void {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			// @ts-ignore
+			document.getElementById("side").style.top = "0";
+			console.log('moved?')
+		} else {
+			// @ts-ignore
+			document.getElementById("side").style.top = "70px";
+		}
+	}
 
 
-    const showSidebar: () => void = () => setSidebar(!sidebar)
+	const showSidebar: () => void = () => setSidebar(!sidebar)
 
-    const goToSection: (num: number) => void = (num : number) => {
-        window.scrollTo({
-            top: num,
-            behavior: "smooth",
-        });
-    };
+	const goToSection: (num: number) => void = (num: number) => {
+		window.scrollTo({
+			top: num,
+			behavior: "smooth",
+		});
+	};
 
-    return (
-        <div>
-        <div id={"side"} className={sidebar ? 'MegaDiv active' : 'MegaDiv'}>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className={'nav-menu-items'}>
-                    {/* tslint:disable-next-line:typedef */}
-                    {SidebarData.map((item, index : number) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Button onClick={() => goToSection(item.nav)} className={'Button'}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-        </div>
-            <div className={sidebar ? 'navbar active' : 'navbar'} onClick={showSidebar}>
-                <Link to='#' className={'menu-bars'}>
-                    <Icon>
-                        <AccessibleForwardOutlined/>
-                    </Icon>
-                </Link>
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<div id={"side"} className={sidebar ? 'MegaDiv active' : 'MegaDiv'}>
+				<nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+					<ul className={'nav-menu-items'}>
+						{/* tslint:disable-next-line:typedef */}
+						{SidebarData.map((item, index: number) => {
+							return (
+								<li key={index} className={item.cName}>
+									<Button onClick={() => goToSection(item.nav)} className={'Button'}>
+										{item.icon}
+										<span>{item.title}</span>
+									</Button>
+								</li>
+							)
+						})}
+					</ul>
+				</nav>
+			</div>
+			<div className={sidebar ? 'navbar active' : 'navbar'} onClick={showSidebar}>
+				<Link to='#' className={'menu-bars'}>
+					<Icon>
+						<AccessibleForwardOutlined/>
+					</Icon>
+				</Link>
+			</div>
+		</div>
+	)
 }
