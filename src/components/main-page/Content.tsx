ed765aa2a89c3b456ce764f40a8d3136c3693b93
Pixerell <PinkDriftsA@protonmail.com>
2023-04-15 22:42:04 +0300

    BT-19 animation cards pt1

--- src/components/main-page/Content.tsx
+++ src/components/main-page/Content.tsx
@@ -22,47 +22,46 @@ function MainContent(): JSX.Element {
 							<h1 className={'HeadTitle'}>Pixerell</h1>
 							<span className={'Line'}></span>
 							<div className={'HeadDescription'}>
-						<span className={'Codetextblock'}>
-							<h3 className={'TextParagraphBold'}><span className={'Tagtext'}>&lt;head&gt;</span>
-								Алексей Ушаков Юрьевич
-							</h3>
-						</span>
-								<span className={'Codetextblock1'}>
-							<p className={'TextParagraph'}>	<span className={'Tagtext'}>&lt;body&gt;</span>Опытный и мотивированный фронт-энд разработчик с практическим опытом работы в
-								<span className={'Tagtext1'}>React, JavaScript, HTML</span>и<span className={'Tagtext1'}>CSS.</span>
-								Увлечен разработкой интерактивных приложений и созданием бесшовных пользовательских интерфейсов.
-							</p>
-						</span>
-								<span className={'Codetextblock2'}>
-							<span className={'TextParagraph'}><span className={'Tagtext'}>&lt;p&gt;</span>Ищу возможность
-								<span className={'Tagtext1'}>стажировки.</span>Для дальнейшего повышения навыков и возможности внести вклад в командный проект.<span className={'Tagtext'}>&lt;/p&gt;</span></span>
-						</span>
-								<span className={'Codetextblock2'}>
-							<ul className={'TextParagraph'}>
-								<span className={'Tagtext'}>&lt;skills&gt;</span>
-								В мои навыки входит:
-										<li>Уверенный фронт стек JavaScript, HTML5 и CSS3</li>
-									<li>Фреймворк React c использованием библиотек<span className={'Tagtext1'}>Redux, Axios</span>и<span className={'Tagtext1'}>Material-UI</span></li>
-									<li>Опыт работы в команде с системами управления версий такими как<span className={'Tagtext1'}>Git</span></li>
-									<li>Дополнительное знание языков C#, Python и расширения<span className={'Tagtext1'}>Typescript</span></li>
-									<li>Опыт работы с бэк-эндом и его внедрения, включая знание<span className={'Tagtext1'}>SQL и LINQ</span></li>
-									<li>Оффициальный<span className={'Tagtext1'}>Дрейнер</span>с уровнем английского<span className={'Tagtext1'}>C1</span></li>
-								<span className={'Tagtext'}>&lt;/skills&gt;</span>
-							</ul>
-						</span>
+								<span className={'Codetextblock'}>
+									<h3 className={'TextParagraphBold'}><span className={'Tagtext'}>&lt;head&gt;</span>
+										Алексей Ушаков Юрьевич
+									</h3>
+								</span>
+										<span className={'Codetextblock1'}>
+									<p className={'TextParagraph'}>	<span className={'Tagtext'}>&lt;body&gt;</span>Опытный и мотивированный фронт-энд разработчик с практическим опытом работы в
+										<span className={'Tagtext1'}>React, JavaScript, HTML</span>и<span className={'Tagtext1'}>CSS.</span>
+										Увлечен разработкой интерактивных приложений и созданием бесшовных пользовательских интерфейсов.
+									</p>
+								</span>
+										<span className={'Codetextblock2'}>
+									<span className={'TextParagraph'}><span className={'Tagtext'}>&lt;p&gt;</span>Ищу возможность
+										<span className={'Tagtext1'}>стажировки.</span>Для дальнейшего повышения навыков и возможности внести вклад в командный проект.<span className={'Tagtext'}>&lt;/p&gt;</span></span>
+								</span>
+										<span className={'Codetextblock2'}>
+									<ul className={'TextParagraph'}>
+										<span className={'Tagtext'}>&lt;skills&gt;</span>
+										В мои навыки входит:
+												<li>Уверенный фронт стек JavaScript, HTML5 и CSS3</li>
+											<li>Фреймворк React c использованием библиотек<span className={'Tagtext1'}>Redux, Axios</span>и<span className={'Tagtext1'}>Material-UI</span></li>
+											<li>Опыт работы в команде с системами управления версий такими как<span className={'Tagtext1'}>Git</span></li>
+											<li>Дополнительное знание языков C#, Python и расширения<span className={'Tagtext1'}>Typescript</span></li>
+											<li>Опыт работы с бэк-эндом и его внедрения, включая знание<span className={'Tagtext1'}>SQL и LINQ</span></li>
+											<li>Оффициальный<span className={'Tagtext1'}>Дрейнер</span>с уровнем английского<span className={'Tagtext1'}>C1</span></li>
+										<span className={'Tagtext'}>&lt;/skills&gt;</span>
+									</ul>
+								</span>
 
+										<span className={'Codetextblock1'}>
+									<p className={'TextParagraph'}>
+									<span className={'Tagtext'}>&lt;/body&gt;</span>
+									</p>
+								</span>
 								<span className={'Codetextblock1'}>
-							<p className={'TextParagraph'}>
-							<span className={'Tagtext'}>&lt;/body&gt;</span>
-							</p>
-						</span>
-
-								<span className={'Codetextblock1'}>
-							<h4 className={'TextParagraph'}>
-							<span className={'Tagtext'}>&lt;projects&gt;</span>
-								Список моих проектов:
-							</h4>
-						</span>
+									<h4 className={'TextParagraph'}>
+									<span className={'Tagtext'}>&lt;projects&gt;</span>
+										Список моих проектов:
+									</h4>
+								</span>
 							</div>
 						</div>
 						<div className={"SideImaging"}>
@@ -73,7 +72,7 @@ function MainContent(): JSX.Element {
 						</div>
 					</div>
 					<div className={"cardWrapper"}>
-						<Box className={"boxCards"} padding={2} sx={{marginLeft: '12%', marginRight:'12%',}} >
+						<Box className={"boxCards"} padding={2} sx={{marginLeft: '16%', marginRight:'16%',}} >
 							<Grid container spacing={16} sx={{marginBottom: 0, width: 'auto', padding: 0}} >
 								{/* tslint:disable-next-line:typedef */}
 								{cardsN.map(cardN => (
@@ -83,6 +82,21 @@ function MainContent(): JSX.Element {
 								))}
 							</Grid>
 						</Box>
+					</div>
+					<div className={'Bottomclass'}>
+						<div className={'BottomDescription'}>
+
+							<span className={'Codetextblock1'}>
+								<h4 className={'TextParagraph'}>
+									<span className={'Tagtext'}>&lt;/projects&gt;</span>
+								</h4>
+							</span>
+								<span className={'Codetextblock'}>
+								<h3 className={'TextParagraphBold'}>
+									<span className={'Tagtext'}>&lt;/head&gt;</span>
+								</h3>
+							</span>
+						</div>
 					</div>
 				</div>
 
--- src/components/main-page/projectCard/ProjectCard.tsx
+++ src/components/main-page/projectCard/ProjectCard.tsx
@@ -1,22 +1,50 @@
 import {Card, CardContent, CardMedia, Typography} from '@mui/material'
+import React, {useState} from "react";
 import albumDefaultPhoto from "../../../workingImages/soundcloud_Icon.png";
 import './ProjectCard.scss'
 
 export default function ProjectCard({card}:any): JSX.Element {
+
+    const [isHovering, setIsHovering] = useState(false);
+
+    // tslint:disable-next-line:typedef
+    let hoverTimeout;
+
+    const handleMouseEnter: () => void = () => {
+        hoverTimeout = setTimeout(() => {
+            setIsHovering(true);
+            console.log("hover")
+        }, 450)
+    };
+
+    const handleMouseLeave: () => void = () => {
+        clearTimeout(hoverTimeout);
+        setIsHovering(false);
+        console.log("unhover")
+
+    };
+    
+
     return (
         <div className="projectCard">
-            <Card elevation={2} className="cardClass" sx={{ minWidth: 380, opacity: 0.95, padding: 0, margin:0, }}  >
+            <Card elevation={2} className="cardClass" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <div className="projectCardBody">
                     <CardContent className="cardContents">
                         <Typography gutterBottom variant="h5" component="div" >
                             {card.projectName}
                         </Typography>
                     </CardContent>
-                    <CardMedia className='cardMedia'
-                        sx={{ height: 300, width: '100%'  }}
-                        image={card.projectImg !== null ? card.projectImg : albumDefaultPhoto}
-                        title={card.projectName}
-                    />
+                    <div className='contentWrapper'>
+                        <Typography variant="body2" className={`smallDescText ${isHovering ? "animate" : ""}`}>
+                            {card.projectSmallDesc}
+                        </Typography>
+                        <span className={`cardMedia cardMediaOverlay blackTint ${isHovering ? "animate1" : ""}`} />
+                        <img alt="cover image card" className={`cardMedia cardMediaOverlay img ${isHovering ? "animate1" : ""}`} src={card.projectImg !== null ? card.projectImg : albumDefaultPhoto}/>
+                        <CardMedia className='cardMedia'
+                                   image={card.projectImg !== null ? card.projectImg : albumDefaultPhoto}
+                                   title={card.projectName}
+                        />
+                    </div>
                 </div>
             </Card>
         </div>
