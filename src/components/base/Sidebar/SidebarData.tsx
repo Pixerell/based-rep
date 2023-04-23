2bd86a97a8593f17049d91f2be12d2bbcd7a044d
Pixerell <PinkDriftsA@protonmail.com>
2023-04-23 21:38:44 +0300

    BT-18 peopleCards finish

--- src/components/base/Sidebar/SidebarData.tsx
+++ src/components/base/Sidebar/SidebarData.tsx
@@ -1,27 +1,18 @@
-import {Headphones, Movie, PsychologyAlt} from "@mui/icons-material";
+import {Headphones, PsychologyAlt} from "@mui/icons-material";
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 import {Icon} from "@mui/material";
 import React from "react";
 
 // tslint:disable-next-line:typedef
 export const SidebarData  = [
     {
-        title: 'Moosic',
+        title: 'Games',
         path: '/base',
         icon:   <Icon>
-                    <Headphones/>
+                    <VideogameAssetIcon/>
                 </Icon>,
         cName: 'nav-text',
         nav: 0
-    },
-    {
-        title: 'Films',
-        path: '/base',
-        icon:   <Icon>
-            <Movie/>
-        </Icon>,
-        cName: 'nav-text',
-        nav: 400
-
     },
     {
         title: 'People',
@@ -30,8 +21,17 @@ export const SidebarData  = [
             <PsychologyAlt/>
         </Icon>,
         cName: 'nav-text',
-        nav: 800
+        nav: 700
 
+    },
+    {
+        title: 'Moosic',
+        path: '/base',
+        icon:   <Icon>
+            <Headphones/>
+        </Icon>,
+        cName: 'nav-text',
+        nav: 2000
     },
 
 
