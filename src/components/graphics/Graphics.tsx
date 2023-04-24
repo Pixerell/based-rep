cd05b70ed9d2c6e6cc4f1b801be2225e7bcfc83f
Pixerell <PinkDriftsA@protonmail.com>
2023-04-24 21:54:50 +0300

    BT - UKNOWN GRAPHICS START LINE CHART ADDED

--- src/components/graphics/Graphics.tsx
+++ src/components/graphics/Graphics.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import LineChart from "./charts/lineChart";
+import './Graphics.scss';
 
 
 export default function Graphics() : JSX.Element {
-	return <div>
-		graphics
-	</div>
+
+	return (
+		<div className={'bgBase1'}>
+			<span className="lineSection">
+				<h1 className={'chartHead'}>
+				  Shoegaze-Stats
+				  <div className={'chartInfo'}>
+					  Этот график показывает развитие шугейза с 1990 по 2002 года
+				  </div>
+				</h1>
+				<div className="lineWrapper">
+					<LineChart/>
+				</div>
+			</span>
+		</div>
+	)
 }
